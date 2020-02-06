1b8244659d52cb23bfdb622ee6e0fa8a3446993e
Zac Uslianer <zacuslianer@gmail.com>
2020-02-06 15:13:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,18 +5,18 @@ require 'pry'
 
 def directors_totals(nds)
   pp nds
-  row_index = 0
+  director_index = 0
   result = {}
-  while row_index < nds.count do
+  while director_index < nds.count do
     total = 0 
-    director_name = nds[row_index][:name]
-    column_index = 0 
-    while column_index < nds[row_index][:movies].count do 
-      total += nds[row_index][:movies][column_index][:worldwide_gross]
-      column_index += 1 
+    director_name = nds[director_index][:name]
+    movie_index = 0 
+    while movie_index < nds[director_index][:movies].count do 
+      total += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
     end 
     result[director_name] = total 
-    row_index += 1 
+    director_index += 1 
   end
    result
 end
